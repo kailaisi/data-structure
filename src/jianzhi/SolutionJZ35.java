823dc6a5048b84d8cd89c41ca0a87928a036c265
kailaisii <541018378@qq.com>
2020-07-05 09:56:23 +0800

    剑指offer35题完成

--- src/jianzhi/SolutionJZ35.java
+++ src/jianzhi/SolutionJZ35.java
@@ -17,25 +17,70 @@ import java.util.Collections;
  *      输出： 7
  * 知识点：数组
  * 解题思路：
- *   数组遍历，每放进去一个数据，看该数据，在前面的所有数据中的排序位置。然后就可以确定增加当前位置的数字，所能组成的逆序对。
+ *  分治方法。先将数组进行拆分。
+ *  然后将其排序，
+ *  排序后的两个数组再合并到一起（通过排序）。在这个过程中统计逆序对
  */
 public class SolutionJZ35 {
     public static void main(String[] args) {
         int[] ints = {7, 2, 4, 3, 1, 6, 5, 0};
         int[] aints={1,2,3,4,5,6,7,0};
-        System.out.println(InversePairs(aints));
+        System.out.println(InversePairs(ints));
     }
     public static int InversePairs(int [] array) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int count=0;
-        for(int i=0;i<array.length;i++){
-            int num = array[i];
-            int i1 = binarySearch(list, list.size(),num);
-            list.add(-i1,num);
-            count+=(list.size()-1+i1);
+        if(array==null ||array.length<=1){
+            return 0;
         }
-        return count%1000000007;
+        int[] copy=new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            copy[i]=array[i];
+        }
+        return InversePairsCore(array,copy,0,array.length-1);
     }
+
+    private static int InversePairsCore(int[] array, int[] copy, int low, int high) {
+        if(low==high){
+            return 0;
+        }
+        int mid=(low+high)>>1;
+        //左侧的数量
+        int leftCount=InversePairsCore(array,copy,low,mid)%1000000007;
+        //右侧的数量
+        int rightCount=InversePairsCore(array,copy,mid+1,high)%1000000007;
+        //计算二者合并成新的数组时，产生的逆序对数量
+        int count=0;
+        int i=mid;
+        int j=high;
+        int locCopy=high;
+        //排序的时候，依次从最后一位取出数据，然后放到copy数组中，在这个过程中，计算逆序对数量
+        while (i>=low && j>mid){
+            if(array[i]>array[j]){//这时候，i比mid~j之间的都要大。
+                //这里原来使用了mid+1-j。其实是重复了。因为如果low-mid是[2,3,4]，mid-heigh是[0,1]。在计算3的时候，mid+1-j。计算了4比0大。再计算2时，仍然会统计4比0大。所以会存在重复问题
+                count+=(j-mid);
+                copy[locCopy]=array[i];//将i放到顺序数组的最后
+                locCopy--;
+                i--;
+                if(count>=1000000007){
+                    count%=1000000007;
+                }
+            }else{//
+                copy[locCopy--]=array[j--];
+            }
+        }
+        //到这里为止，可能存在，i或者j遍历完了，但是另一个仍然还存在数据。所以继续。
+        for (;i>=low;i--){
+            copy[locCopy--]=array[i];
+        }
+        for (; j>mid ; j--) {
+            copy[locCopy--]=array[j];
+        }
+        //到这里为止，copy已经是顺序的数组，从小到大。然后将copy数组赋值给array数据，将其low，到high的位置从小到大排序
+        for(int k=low;k<=high;k++){
+            array[k]=copy[k];
+        }
+        return (leftCount+rightCount+count)%1000000007;
+    }
+
     // 二分查找法，查找位置
     static int binarySearch(ArrayList<Integer> array, int size, int value) {
         //定义低位
