e0bd5eb714b65c363d02c242690074111fdc8181
wujinxiang <wujinxiang@tcsl.com.cn>
2020-07-10 08:50:24 +0800

    并查集基础实现  quickfind

--- src/datalearning/unionfind/QuickFind.java
+++ src/datalearning/unionfind/QuickFind.java
@@ -1,7 +1,7 @@
 package datalearning.unionfind;
 
 /**
- * 描述：快速查询
+ * 描述：快速查询，最简单的并查集，基于数组的实现。
  * <p/>作者：wu
  * <br/>创建时间：2020/7/9 9:24
  */
@@ -17,17 +17,34 @@ class QuickFind implements UF {
     }
 
     @Override
-    public boolean isConnected(int p, int q) {
-        return false;
+    public boolean isConnected(int p, int q) {//O(1)级别
+        return find(p)==find(q);
     }
 
     @Override
-    public void connect(int p, int q) {
+    public void connect(int p, int q) {//O(n)级别
+        int pID = find(p);
+        int qID = find(q);
+        if(pID==qID){
+            return;
+        }
+        for(int i=0;i<ids.length;i++){
+            if(ids[i]==qID){
+                ids[i]=pID;
+            }
+        }
+    }
 
+    //查找元素p所对应的集合编号
+    private int find(int p){
+        if(p<0 ||p>=ids.length){
+            throw new IllegalArgumentException("数组越界");
+        }
+        return ids[p];
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return ids.length;
     }
 }
