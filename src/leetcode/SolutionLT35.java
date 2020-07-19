d29d194487177e246c01ac6ac099eb9f9b2f6df9
kailaisii <541018378@qq.com>
2020-07-19 22:49:40 +0800

    LT35完成

--- src/leetcode/SolutionLT35.java
+++ src/leetcode/SolutionLT35.java
@@ -15,27 +15,28 @@ package leetcode;
  * 示例 4:
  * 输入: [1,3,5,6], 0
  * 输出: 0
+ * 解题思路：这个其实是Android中的SpraseArray的遍历方法
  */
 class SolutionLT35 {
-//todo 未完成
     public static void main(String[] args) {
         int[] a = new int[]{1, 3, 5, 6};
-        int i = new SolutionLT35().searchInsert(a, 3);
+        int i = new SolutionLT35().searchInsert(a, 0);
         System.out.println(i);
     }
 
     public int searchInsert(int[] nums, int target) {
-        int i = 0, j = nums.length-1, mid = 0;
-        while (i < j) {
-            mid = (i + j) / 2 + 1;
-            if (nums[mid] == target) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int mid = (i + j) >>> 1;
+            int midVal = nums[mid];
+            if (midVal == target) {
                 return mid;
-            } else if (nums[mid] < target) {
-                j = mid - 1;
+            } else if (midVal < target) {
+                i = mid +1;
             } else {
-                i = mid + 1;
+                j = mid - 1;
             }
         }
-        return mid;
+        return i;
     }
 }
