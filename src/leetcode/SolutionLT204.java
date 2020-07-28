d7b9de0645097d7569b1dbb40af1a3cc5fd4484b
wujinxiang <wujinxiang@tcsl.com.cn>
2020-07-28 09:24:14 +0800

    力扣204

---
+++ src/leetcode/SolutionLT204.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+/**
+ * 统计所有小于非负整数 n 的质数的数量。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 10
+ * 输出: 4
+ * 解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+ */
+class SolutionLT204 {
+    //todo
+    public static void main(String[] args) {
+
+    }
+
+    public int countPrimes(int n) {
+        int count=0
+        for(int i=1;i<n;i+=2){
+
+        }
+        return count;
+    }
+
+    private boolean isPrimes(int n){
+        for(int i=2;i<n/2;i++){
+            if(n%i==0){
+                ret
+            }
+        }
+    }
+}
