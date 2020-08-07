79893e02809986d4b9c49bf97921a036931c9eed
wujinxiang <wujinxiang@tcsl.com.cn>
2020-08-07 09:28:50 +0800

    new:计数排序

---
+++ src/sort/CountSort.kt
@@ -0,0 +1,14 @@
+package sort
+
+/**
+ *描述：计数排序。如果数组的数据是有一定范围的，而且取值范围不是特别大的情况下，可以通过使用计数排序来进行处理
+ *  计数排序：创建一个数组，数组下标对应具体的数据，数组的值代表其出现的次数。通过统计每个数据产生的次数。
+ *      然后通过遍历数组，根据其出现的次数重新进行输出
+ *<p/>作者：kailaisi
+ *<br/>创建时间：2020/8/7 9:25
+ */
+class CountSort : Sort {
+    override fun sort(intArray: IntArray) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
