c43ec19f06fc2b0cd7823e9610003db7d376216c
kailaisii <541018378@qq.com>
2021-01-23 10:12:45 +0800

    力扣2402种思路：二分查找法

--- src/leetcode/SolutionLT240Forth.java
+++ src/leetcode/SolutionLT240Forth.java
@@ -32,7 +32,9 @@ package leetcode;
  * <p>
  * 相似题目：74
  * <p>
- * 解题思路：使用二分法
+ * 解题思路：使用二分法。对于行或者列数据，都是有序的。所以我们可以通过对行或者列进行二分搜索来处理数据。还有一个比较好的地方是，对于[i][j]位置，右下方都是比他大的。
+ * 当我们按照对角线找到比target数据大的时候，就没必要再向下查找了。
+ * 这时候，从0行开始到index，按照行和列进行二分查找即可。
  * 时间复杂度：O(m*n)
  */
 class SolutionLT240Forth {
@@ -48,15 +50,57 @@ class SolutionLT240Forth {
     public boolean searchMatrix(int[][] matrix, int target) {
         int height = matrix.length;
         int width = matrix[0].length;
-        int j = 0;
-        int i = height - 1;
-        while (j <width && i >= 0) {
-            if (matrix[i][j] == target) {
+        int right = Math.max(height-1, width-1);
+        //查找对角线上第一个>target的坐标数据
+        int left = 0;
+        int mid = 0;
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (matrix[mid][mid] == target) {
                 return true;
-            } else if (matrix[i][j] > target) {
-                i--;
+            } else if (matrix[mid][mid] > target) {
+                right = mid;
             } else {
-                j++;
+                left = mid + 1;
+            }
+        }
+        //从0-mid二分搜索数据
+        for (int i = 0; i <= left; i++) {
+            boolean b = bineryRowSearch(matrix, i, width-1, target);
+            boolean b1 = bineryColSearch(matrix, i, height-1, target);
+            if (b||b1){
+                return true;
+            }
+        }
+        return bineryRowSearch(matrix, left, left, target);
+    }
+
+    private boolean bineryColSearch(int[][] matrix, int left, int right, int target) {
+        int col=left;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (matrix[mid][col] == target) {
+                return true;
+            } else if (matrix[mid][col] > target) {
+                right = mid-1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    //行搜索
+    private boolean bineryRowSearch(int[][] matrix, int left, int right, int target) {
+        int row=left;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] > target) {
+                right = mid-1;
+            } else {
+                left = mid + 1;
             }
         }
         return false;
