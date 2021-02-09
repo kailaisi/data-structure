383c39e92c1ef07e8f765bf7f7ef5c4326ad2156
kailaisii <541018378@qq.com>
2021-02-09 22:59:37 +0800

    力扣563：递归算法

---
+++ src/leetcode/SolutionLT563.java
@@ -0,0 +1,88 @@
+package leetcode;
+
+/**
+ * 563. 二叉树的坡度  https://leetcode-cn.com/problems/binary-tree-tilt/
+ * 给定一个二叉树，计算 整个树 的坡度 。
+ * 一个树的 节点的坡度 定义即为，该节点左子树的节点之和和右子树节点之和的 差的绝对值 。如果没有左子树的话，左子树的节点之和为 0 ；没有右子树的话也是一样。空结点的坡度是 0 。
+ * 整个树 的坡度就是其所有节点的坡度之和。
+ * 示例 1：
+ * 输入：root = [1,2,3]
+ * 输出：1
+ * 解释：
+ * 节点 2 的坡度：|0-0| = 0（没有子节点）
+ * 节点 3 的坡度：|0-0| = 0（没有子节点）
+ * 节点 1 的坡度：|2-3| = 1（左子树就是左子节点，所以和是 2 ；右子树就是右子节点，所以和是 3 ）
+ * 坡度总和：0 + 0 + 1 = 1
+ * 示例 2：
+ * <p>
+ * 输入：root = [4,2,9,3,5,null,7]
+ * 输出：15
+ * 解释：
+ * 节点 3 的坡度：|0-0| = 0（没有子节点）
+ * 节点 5 的坡度：|0-0| = 0（没有子节点）
+ * 节点 7 的坡度：|0-0| = 0（没有子节点）
+ * 节点 2 的坡度：|3-5| = 2（左子树就是左子节点，所以和是 3 ；右子树就是右子节点，所以和是 5 ）
+ * 节点 9 的坡度：|0-7| = 7（没有左子树，所以和是 0 ；右子树正好是右子节点，所以和是 7 ）
+ * 节点 4 的坡度：|(3+5+2)-(9+7)| = |10-16| = 6（左子树值为 3、5 和 2 ，和是 10 ；右子树值为 9 和 7 ，和是 16 ）
+ * 坡度总和：0 + 0 + 0 + 2 + 7 + 6 = 15
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：root = [21,7,14,1,1,2,2,3,3]
+ * 输出：9
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数目的范围在 [0, 104] 内
+ * -1000 <= Node.val <= 1000
+ * 解题思路：我们要计算坡度的总和，那么就需要计算每个节点的坡度，然后将相应的坡度值进行求和处理。
+ * 坡度如何求呢？就是求左右位置的和。这时候我们可以使用递归方式计算所有的和。
+ * 而对于root，计算它及其节点的和的过程中，就必须知道左右节点和，而知道左右节点的和，就知道了节点的坡度了。通过不断的递归，得到结果
+ * 时间复杂度：O(n).遍历了一次所有的节点
+ * 空爱你复杂度：O（n）
+ */
+class SolutionLT563 {
+    public static void main(String[] args) {
+        SolutionLT563 lt167 = new SolutionLT563();
+        int[] numbers = new int[]{1, 2, 3, 4};
+    }
+
+    int tilt = 0;
+
+    public int findTilt(TreeNode root) {
+        cal(root);
+        return tilt;
+    }
+
+    //计算root及其左右结算的和
+    private int cal(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = cal(root.left);
+        int righgt = cal(root.right);
+        //这知道了左右节点的和了，那么就可以计算该节点的
+        tilt += Math.abs(left - righgt);
+        return left + righgt + root.val;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
