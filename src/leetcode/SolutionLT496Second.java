cbd9fba544dda7895c85d7603d11e27e11eb4db4
wujinxiang <541018378@qq.com>
2021-06-22 10:41:46 +0800

    力扣496。单调栈未完成

--- src/leetcode/SolutionLT496Second.java
+++ src/leetcode/SolutionLT496Second.java
@@ -1,79 +1,77 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Stack;
 
 /**
  * 496. 下一个更大元素 I
  * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。
- *
+ * <p>
  * 请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。
- *
+ * <p>
  * nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 -1 。
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
  * 输出: [-1,3,-1]
  * 解释:
- *     对于 num1 中的数字 4 ，你无法在第二个数组中找到下一个更大的数字，因此输出 -1 。
- *     对于 num1 中的数字 1 ，第二个数组中数字1右边的下一个较大数字是 3 。
- *     对于 num1 中的数字 2 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+ * 对于 num1 中的数字 4 ，你无法在第二个数组中找到下一个更大的数字，因此输出 -1 。
+ * 对于 num1 中的数字 1 ，第二个数组中数字1右边的下一个较大数字是 3 。
+ * 对于 num1 中的数字 2 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
  * 示例 2:
- *
+ * <p>
  * 输入: nums1 = [2,4], nums2 = [1,2,3,4].
  * 输出: [3,-1]
  * 解释:
- *     对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
- *     对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
- *
- *
+ * 对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+ * 对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+ * <p>
+ * <p>
  * 提示：
- *
+ * <p>
  * 1 <= nums1.length <= nums2.length <= 1000
  * 0 <= nums1[i], nums2[i] <= 104
  * nums1和nums2中所有整数 互不相同
  * nums1 中的所有整数同样出现在 nums2 中
- *
- *
+ * <p>
+ * <p>
  * 进阶：你可以设计一个时间复杂度为 O(nums1.length + nums2.length) 的解决方案吗？
- * 解题思路：暴力法，对每个位置的数据都整体遍历一次
+ * 解题思路：单调栈，对于num2素组，先建立对应的单调栈，然后对于num1进行遍历，获取其在num2中的位置，之后就可以直接互殴去右侧最大的数字了
  * 空间复杂度：O(n^2)
  * 时间复杂度：O(n)
  */
 class SolutionLT496Second {
     public static void main(String[] args) {
-        int[] ints = new SolutionLT496Second().nextGreaterElement(new int[]{4,1,2}, new int[]{1,3,4,2});
+        int[] ints = new SolutionLT496Second().nextGreaterElement(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2});
         System.out.println(Arrays.toString(ints));
-         ints = new SolutionLT496Second().nextGreaterElement(new int[]{2, 4}, new int[]{1, 2, 3, 4});
+        ints = new SolutionLT496Second().nextGreaterElement(new int[]{2, 4}, new int[]{1, 2, 3, 4});
         System.out.println(Arrays.toString(ints));
-
     }
+
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
-        int[] ints = new int[len1];
-        for (int i=0;i<len1;i++){
-            int j=0;
-            /*找到相等的位置*/
-            for (j=0; j < len2; j++) {
-                if (nums2[j]==nums1[i]){
-                    break;
-                }
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < len2; i++) {
+            while (!stack.isEmpty() && nums2[i] > nums2[stack.peek()]) {
+                Integer pop = stack.pop();
+                map.put(pop, i);
             }
-            /*从相等位置开始，找到比他大的位置*/
-            for (; j < len2; j++) {
-                if (nums2[j]>nums1[i]){
-                    ints[i]=nums2[j];
-                    break;
-                }
-            }
-
-            if (j==len2){
-                ints[i]=-1;
+            stack.push(i);
+        }
+        for (int i = 0; i < len1; i++) {
+            Integer integer = map.get(nums1[i]);
+            if (integer == null) {
+                nums1[i] = -1;
+            } else {
+                nums1[i] = nums2[integer];
             }
         }
-        return ints;
+        return nums1;
     }
 }
