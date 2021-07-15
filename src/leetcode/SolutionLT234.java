ed1bd2d3b677a8670bf088e4efb5904ad4cdbf64
wujinxiang <541018378@qq.com>
2021-07-15 10:27:03 +0800

    力扣234。暴力法完成。

--- src/leetcode/SolutionLT234.java
+++ src/leetcode/SolutionLT234.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 /**
@@ -13,34 +14,34 @@ import java.util.Stack;
  * 输出: true
  * 进阶：
  * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
- * 解题思路：判断是否回文，其实相当于从中间位置开始，然后将前后的进行折叠即可。那么是否可以进行快慢指针，将满指针的数据存入到栈中。
- * * 时间复杂度：O（N）：
- * * 空间复杂度：O（1）
+ * 解题思路：暴力法    判断是否回文，其实相当于从中间位置开始，然后将前后的进行折叠即可。
+ * 可以将链表的数据存入数组，然后从中间开始往前往后，逐一进行比较
+ * * 时间复杂度：O（N）：需要一次遍历
+ * * 空间复杂度：O（N）：需要用列表来保存
  */
 class SolutionLT234 {
     public static void main(String[] args) {
         SolutionLT234 lt = new SolutionLT234();
-        System.out.println(lt.isPalindrome(new ListNode(1)));//false
+        System.out.println(lt.isPalindrome(new ListNode(1)));//true
         System.out.println(lt.isPalindrome(new ListNode(1, new ListNode(2))));//false
-        //    System.out.println(lt.isPalindrome(new ListNode(1, new ListNode(2,new ListNode(3)))));//false
-        //   System.out.println(lt.isPalindrome(new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))))));//false
-        System.out.println(lt.isPalindrome(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(2, new ListNode(1)))))));//false
+        System.out.println(lt.isPalindrome(new ListNode(1, new ListNode(2, new ListNode(3)))));//false
+        System.out.println(lt.isPalindrome(new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))))));//true
+        System.out.println(lt.isPalindrome(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(2, new ListNode(1)))))));//true
     }
 
     public boolean isPalindrome(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        Stack<ListNode> stack = new Stack<>();
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            stack.push(slow);
-            slow = slow.next;
+        ArrayList<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
         }
-        while (slow != null) {
-            if (stack.isEmpty() || stack.pop().val != slow.val) {
+        //个数是10   那么就比较position的到i=0-4    9-i
+        //个数是9   那么就比较position的到i=0-3    9-i  0 1 2 3    5  6  7  8
+        int size = list.size();
+        for (int i = 0; i < size / 2; i++) {
+            if (list.get(i).val != list.get(size - i - 1).val) {
                 return false;
             }
-            slow = slow.next;
         }
         return true;
     }
