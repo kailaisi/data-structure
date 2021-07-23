189d6803d310dd5ff0fbef0ae1f4d8a8d56e8b4c
wujinxiang <541018378@qq.com>
2021-07-23 09:03:59 +0800

    力扣160   暴力法、栈、重叠链表方案完成。

--- src/leetcode/SolutionLT160Third.java
+++ src/leetcode/SolutionLT160Third.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import java.util.Stack;
-
 /**
  * 160. 相交链表
  * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
@@ -36,14 +34,16 @@ import java.util.Stack;
  * 如果 listA 和 listB 没有交点，intersectVal 为 0
  * 如果 listA 和 listB 有交点，intersectVal == listA[skipA + 1] == listB[skipB + 1]
  * 进阶：你能否设计一个时间复杂度 O(n) 、仅用 O(1) 内存的解决方案？
- * 解题思路：暴力法，每个节点都全部判断
+ * 解题思路：相交链表功能方法。
+ * 假设公共长度为c，A的长度a+c，B的长度为b+c；当A遍历完之后，指向B的头节点，B遍历完之后，走向A的节点。如果二者相交，那么
+ * a+c+b=b+c+a。所以当走过这个距离的时候，而且就会相遇了。
  * .注意：这里恶心的地方是：判断的是listNode相等，而不是其对应的值相等！！！！！！
- * 时间复杂度：O（N^2）：我们需要将数据进行双重遍历
- * 空间复杂度：O（1）：需要用栈来保存
+ * 时间复杂度：O（N）：我们需要将数据进行两次的遍历，并保存下来
+ * 空间复杂度：O（1）：不会申请任何的新空间
  */
-class SolutionLT160 {
+class SolutionLT160Third {
     public static void main(String[] args) {
-        SolutionLT160 lt = new SolutionLT160();
+        SolutionLT160Third lt = new SolutionLT160Third();
         System.out.println(lt.getIntersectionNode(
                 new ListNode(4, new ListNode(1, new ListNode(8, new ListNode(4, new ListNode(5))))),
                 new ListNode(5, new ListNode(0, new ListNode(1, new ListNode(8, new ListNode(4, new ListNode(5))))))));
@@ -53,18 +53,21 @@ class SolutionLT160 {
         if (headA == null || headB == null) {
             return null;
         }
+        ListNode pa = headA;
         ListNode pb = headB;
-        while (headA != null) {
-            pb = headB;
-            while (pb != null) {
-                if (headA == pb) {
-                    return pb;
-                }
-                pb = pb.next;
+        while (pa != pb) {
+            if (pa != null) {
+                pa = pa.next;
+            } else {
+                pa = headB;
             }
-            headA = headA.next;
+            if (pb != null) {
+                pb = pb.next;
+            } else {
+                pb = headA;
+            }
         }
-        return null;
+        return pa;
     }
 
 
